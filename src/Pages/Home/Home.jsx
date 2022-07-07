5da1b2eb767f4fc60ab220bb7b424c95e50b3d32
Breno Nascimento <satsukebare23@gmail.com>
2022-07-07 16:27:18 -0300

    criando componente Home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Home</h1>
+      </main>
+    );
+  }
+}
+
+export default Home;
